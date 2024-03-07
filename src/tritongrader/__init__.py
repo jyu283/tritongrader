b1b39d02bc8d5298c28394bcc4e31924d8e0f2bc
Jerry Yu <jerryyu01@outlook.com>
2024-03-06 21:54:45 -0800

    logs go to stdout

--- src/tritongrader/__init__.py
+++ src/tritongrader/__init__.py
@@ -1,6 +1,7 @@
+import sys
 import logging
 
 import logging.config
 from tritongrader.autograder import Autograder # noqa
 
-logging.basicConfig(format="[%(asctime)s] %(message)s", level=logging.DEBUG)
+logging.basicConfig(format="[%(asctime)s] %(message)s", level=logging.DEBUG, stream=sys.stdout)
